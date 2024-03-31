c482653a71eb172ca6d7da6b2aa5795c3a2b7208
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2024-04-01 00:59:56 +0200

    Create playRound()
    a function to play one round of rock paper scissors

--- script.js
+++ script.js
@@ -16,3 +16,42 @@ function getComputerChoice() {
         break;
     }
 }
+
+/* create playRound() to play one round of rock, paper, scissors */
+
+function playRound(playerSelection, computerSelection) {
+
+    if ( playerSelection === computerSelection ) {
+        console.log("This round is a tie!");
+        console.log("-------------");
+        return 0;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("You Win! Rock beats Scissors");
+        console.log("-------------");
+        return 1;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("You Win! Paper beats Rock");
+        console.log("-------------");
+        return 1;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("You Win! Scissors beats Paper");
+        console.log("-------------");
+        return 1;
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        console.log("You Lose! Paper beats Rock");
+        console.log("-------------");
+        return -1;
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        console.log("You Lose! Scissors beats Paper");
+        console.log("-------------");
+        return -1;
+    } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        console.log("You Lose! Rock beats Scissors");
+        console.log("-------------");
+        return -1;
+    } else {
+        console.log("Error: Please type: rock, paper or scissors.");
+    }
+
+}
+
