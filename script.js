f3df9de94a2504ea362fe9eed42cd84c84189bf7
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2024-05-09 03:49:21 +0300

    Refactor playRound()

--- script.js
+++ script.js
@@ -28,31 +28,15 @@ function playRound(playerSelection, computerSelection) {
         console.log("This round is a tie!");
         console.log("-------------");
         return 0;
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("You Win! Rock beats Scissors");
+    } else if ( (playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper") ) {
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
         console.log("-------------");
         return 1;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You Win! Paper beats Rock");
-        console.log("-------------");
-        return 1;
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("You Win! Scissors beats Paper");
-        console.log("-------------");
-        return 1;
-    } else if (computerSelection === "paper" && playerSelection === "rock") {
+    } else if ( (computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "scissors" && playerSelection === "paper") || (computerSelection === "rock" && playerSelection === "scissors") ) {
         console.log("You Lose! Paper beats Rock");
         console.log("-------------");
         return -1;
-    } else if (computerSelection === "scissors" && playerSelection === "paper") {
-        console.log("You Lose! Scissors beats Paper");
-        console.log("-------------");
-        return -1;
-    } else if (computerSelection === "rock" && playerSelection === "scissors") {
-        console.log("You Lose! Rock beats Scissors");
-        console.log("-------------");
-        return -1;
-    } else {
+    }else {
         console.log("Error: Please type: rock, paper or scissors.");
     }
 
