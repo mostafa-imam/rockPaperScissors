c3b156104f6a03feb8fe0bc49271af39387439a7
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2024-05-11 05:55:13 +0300

    refactor getComputerChoice()

--- script.js
+++ script.js
@@ -1,43 +1,18 @@
-console.log("Welcome!");
-console.log("-------------");
-
-/* Create getComputerChoice() to return a random rock-paper-scissors value */
-
 function getComputerChoice() {
 
-    let randomChoice = Math.floor( Math.random() * 3 );
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor( Math.random() * choices.length )];
+};
 
-    switch (randomChoice) {
-        case 0:
-            return 'rock';
-        break;
-        case 1:
-            return 'paper';
-        break;
-        case 2:
-            return 'scissors';
-        break;
-    }
-}
-
-/* create playRound() to play one round of rock, paper, scissors */
-
-function playRound(playerSelection, computerSelection) {
-
-    if ( playerSelection === computerSelection ) {
-        console.log("This round is a tie!");
-        console.log("-------------");
+function playRound (player, computer) {
+    
+    if (player == computer) {
         return 0;
-    } else if ( (playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper") ) {
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-        console.log("-------------");
+    } else if ( (player == 'rock' && computer == 'scissors') ||
+    (player == 'paper' && computer == 'rock') ||
+    (player == 'scissors' && computer == 'paper') ) {
         return 1;
-    } else if ( (computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "scissors" && playerSelection === "paper") || (computerSelection === "rock" && playerSelection === "scissors") ) {
-        console.log("You Lose! Paper beats Rock");
-        console.log("-------------");
+    } else {
         return -1;
-    }else {
-        console.log("Error: Please type: rock, paper or scissors.");
     }
-
-}
+};
