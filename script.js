9617aca9943ec6639508786790e911a6166efbce
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2024-05-11 05:58:33 +0300

    feat: Update game to play with buttons
    Switch game play from console to buttons

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const resultDiv = document.querySelector('.result');
+
+const buttons = document.querySelectorAll('.btn');
+
+let resultPlayer = 0;
+let resultComputer = 0;
+
+buttons.forEach( (button) => {
+
+    button.addEventListener("click", () => {
+
+        const playerSelection = button.dataset.choice;
+        const computerSelection = getComputerChoice();
+
+        let result = playRound(playerSelection, computerSelection);
+
+        if (result == 0) {
+            resultDiv.textContent = "This Round is a Tie";
+        } else if (result == 1) {
+            resultDiv.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+            resultPlayer++;
+        } else if (result == -1) {
+            resultDiv.textContent = `You Lost! ${computerSelection} beats ${playerSelection}`;
+            resultComputer++;
+        }
+    })
+})
+
 function getComputerChoice() {
 
     const choices = ["rock", "paper", "scissors"];
@@ -5,7 +33,7 @@ function getComputerChoice() {
 };
 
 function playRound (player, computer) {
-    
+
     if (player == computer) {
         return 0;
     } else if ( (player == 'rock' && computer == 'scissors') ||
